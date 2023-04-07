7b7b661923ac1b94bb413a9f4a7c90feb8607dd9
Alberto Fanton <alberto.fanton@protonmail.com>
2023-04-07 20:40:38 +0200

    Split setups

--- setup_root.sh
+++ setup_root.sh
@@ -15,16 +15,6 @@ substituters = https://cache.nixos.org https://cache.iog.io
 trusted-public-keys = hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ= cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=
 EOF
 
+cp ./setup_user.sh /home/albertodvp/
 
-su - albertodvp
-git clone https://github.com/input-output-hk/cardano-node
-cd cardano-node
-
-tmux start-server  
-tmux new-session -d -s node
-tmux new-window -t node:1  -n "Node"
-tmux send-keys -t node:1 "nix build .#cardano-node -o cardano-node-build --accept-flake-config" C-m
-tmux new-window -t node:2  -n "CLI"
-tmux send-keys -t node:2 "nix build .#cardano-cli -o cardano-cli-build --accept-flake-config" C-m
-
-tmux attach-session -t node:1
+su -c setup.sh albertodvp
---
+++ setup_user.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git clone https://github.com/input-output-hk/cardano-node
+cd cardano-node
+
+tmux start-server  
+tmux new-session -d -s node
+tmux new-window -t node:1  -n "Node"
+tmux send-keys -t node:1 "nix build .#cardano-node -o cardano-node-build --accept-flake-config" C-m
+tmux new-window -t node:2  -n "CLI"
+tmux send-keys -t node:2 "nix build .#cardano-cli -o cardano-cli-build --accept-flake-config" C-m
+
+tmux attach-session -t node:1
