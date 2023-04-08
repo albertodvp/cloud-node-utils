9fa0ab93183138b33ecd03838e3a6f0738614a31
Alberto Fanton <alberto.fanton@protonmail.com>
2023-04-08 21:54:28 +0200

    Preview script

--- scripts/setup_run_testnet_preview.sh
+++ scripts/setup_run_testnet_preview.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-CONFIG="$CARDANO_DATA/config/preview"
-
-mkdir "$CONFIG" -p
-cd "$CONFIG" || exit 1
+cd "$CARDANO_CONFIG_PREVIEW" || exit 1
 
 curl -O -J https://book.world.dev.cardano.org/environments/preview/config.json
 curl -O -J https://book.world.dev.cardano.org/environments/preview/db-sync-config.json
@@ -12,11 +9,10 @@ curl -O -J https://book.world.dev.cardano.org/environments/preview/byron-genesis
 curl -O -J https://book.world.dev.cardano.org/environments/preview/shelley-genesis.json
 curl -O -J https://book.world.dev.cardano.org/environments/preview/alonzo-genesis.json
 
-
 cardano-node run \
-             --topology "$CONFIG/topology.json" \
+             --topology "$CARDANO_CONFIG_PREVIEW/topology.json" \
              --database-path "$CARDANO_DATA" \
-             --socket-path "$CARDANO_DATA/node.socket" \
+             --socket-path "$CARDANO_CONFIG_PREVIEW/node.socket" \
              --host-addr 0.0.0.0 \
              --port 3001 \
-             --config "$CONFIG/config.json"
+             --config "$CARDANO_CONFIG_PREVIEW/config.json"
--- scripts/setup_user.sh
+++ scripts/setup_user.sh
@@ -3,17 +3,24 @@
 BUILD_NODE="nix build .#cardano-node -o cardano-node-build --accept-flake-config"
 BUILD_CLI="nix build .#cardano-cli -o cardano-cli-build --accept-flake-config"
 CARDANO_DATA=$HOME/data/cardano/db
-
+CARDANO_CONFIG_PREVIEW="$CARDANO_DATA/config/preview"
+CARDANO_CONFIG_MAINNET="$CARDANO_DATA/config/mainnet"
 git clone https://github.com/input-output-hk/cardano-node 
 cd cardano-node || exit 1
 
 mkdir "$CARDANO_DATA" -p
+mkdir "$CARDANO_CONFIG_PREVIEW" -p
+mkdir "$CARDANO_CONFIG_MAINNET" -p
 
-cat <<EOF | tee ~/.bashrc
+cat <<EOF | tee -a ~/.bashrc
 export CARDANO_DATA=$CARDANO_DATA
+export CARDANO_CONFIG_PREVIEW=$CARDANO_CONFIG_PREVIEW
+export CARDANO_CONFIG_MAINNET=$CARDANO_CONFIG_MAINNET
 alias cardano-node=$HOME/cardano-node/cardano-node-build/bin/cardano-node
 alias cardano-cli=$HOME/cardano-node/cardano-cli-build/bin/cardano-cli
 EOF
+
+source "$HOME/.bashrc"
 
 tmux start-server  
 tmux new-session -d -s node
